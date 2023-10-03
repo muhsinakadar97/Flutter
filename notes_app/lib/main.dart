2efb088d664aad29d63e9e58508d986a63a46ef5
muhsinakadar97 <muhsinakadar97@gmail.com>
2023-10-03 16:58:17 +0530

    some missing

---
+++ notes_app/lib/main.dart
@@ -0,0 +1,41 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:notes_app/FifthScreen.dart';
+import 'package:notes_app/slidable_themesScreen.dart';
+import 'HomeScreen.dart';
+import 'SecondScreen.dart';
+import 'ThirdScreen.dart';
+import 'FourthScreen.dart';
+import 'ListScreen.dart';
+import 'ChartScreen.dart';
+import 'SixthScreen.dart';
+import 'SplashScreen.dart';
+import 'SeventhScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Example',
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        '/': (context) => HomeScreen(),
+        '/second': (context) => SecondScreen(),
+        '/third': (context) => ThirdScreen(),
+        '/fourth': (context) => FourthScreen(),
+        '/list' :(context) => ListScreen(),
+        '/chart' : (context) => ChartScreen(),
+        '/slidable' : (context) => SlidableDemo(),
+        '/fifth' : (context) => FifthScreen(),
+        '/sixth' : (context) => SixthScreen(),
+        '/seventh' : (context) => SeventhScreen(),
+        
+      },
+    );
+  }
+}
